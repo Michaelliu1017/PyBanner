af8092d01f5b5e4dcd8464a4f46dc975fd00938b
Michaelliu1017 <liuguoliang1017@gmail.com>
2026-04-03 14:56:08 -0400

    created a package

--- src/pyBanner/__main__.py
+++ src/pyBanner/__main__.py
@@ -1,6 +1,7 @@
 from banner import banner, info, other
 
-# 启动时选择想要的组合
-banner(0)        # 噪点浮现 logo
-other(1, title="RRAgent")   # 文字从噪点浮现
+# Try it
+# read the Info in banner.py
+banner(0)
+other(1, title="RRAgent")
 info(0, project="RRAgent", environment="Dev", version="1.0")

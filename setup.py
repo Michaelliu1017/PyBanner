69406e34493c74b12ff6ec1e5b3382f47125f1bf
Michaelliu1017 <liuguoliang1017@gmail.com>
2026-04-03 15:20:39 -0400

    fix package structure

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pybanner",
     version="1.0.0",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     python_requires=">=3.9",
 )

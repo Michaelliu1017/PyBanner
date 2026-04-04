3795fd4ccbdfcb1958b390b3593f13f5b10941ac
Michaelliu1017 <liuguoliang1017@gmail.com>
2026-04-03 21:53:39 -0400

    .

--- src/pyBanner/__main__.py
+++ src/pyBanner/__main__.py
@@ -2,7 +2,7 @@ from banner import banner, info, effect
 
 # Try it
 # read the Info in banner.py
-banner(8)
+banner(7)
 effect(1, title="Project Title")
 info(0, project="R&R Agent", environment="Dev", version="1.0")
 

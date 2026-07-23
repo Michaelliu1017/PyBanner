c3534ca6098ab09748f4279d0fcee6af5df1e90c
Michaelliu1017 <liuguoliang1017@gmail.com>
2026-07-23 15:27:43 +0800

    add owl logos and update banner

--- src/pyBanner/__main__.py
+++ src/pyBanner/__main__.py
@@ -2,8 +2,12 @@ from banner import banner, info, effect
 
 # Try it
 # read the Info in banner.py
+for i in range(20):
+    print()
 banner(6)
-effect(1,description="A self-evolving wargame AI agent ")
-info(0, project="R&R Agent", environment="Dev", version="1.0")
+
+#effect(1,description="A self-evolving wargame AI agent ")
+
+#info(0, project="R&R Agent", environment="Dev", version="1.0")
 
 
